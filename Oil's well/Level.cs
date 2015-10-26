1c1ce44c7901f5de5062bfff49f9a2b34f30cfc6
Oriannanas <marijn_10_marijn@hotmail.com>
2015-10-26 22:33:18 +0100

    Final

---
+++ Oil's well/Level.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Oils_well
+{
+    class Level
+    {
+        private int levelHeight;
+        private int levelWidth;
+        private List<string> grid = new List<string>();
+
+        public int LevelHeight
+        {
+            get
+            {
+                return levelHeight;
+            }
+        }
+        public int LevelWidth
+        {
+            get
+            {
+                return levelWidth;
+            }
+        }
+
+        public List<string> Grid
+        {
+            get
+            {
+                return grid;
+            }
+        }
+
+        public Level(List<string> grid)
+        {
+            this.grid = grid;
+            this.levelWidth = grid[0].ToCharArray().Length;
+            this.levelHeight = grid.Count;
+        }
+    }
+}
